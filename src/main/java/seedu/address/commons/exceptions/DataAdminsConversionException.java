818e603d19dc813abf01e240589ffe15c4fd6768
futia <ftygary@gmail.com>
2018-10-16 02:06:34 +0800

    update not to wrongly delete

---
+++ src/main/java/seedu/address/commons/exceptions/DataAdminsConversionException.java
@@ -0,0 +1,9 @@
+package seedu.address.commons.exceptions;
+/**
+ * Represents an error during conversion of admin data from xml
+ */
+public class DataAdminsConversionException extends Exception {
+    public DataAdminsConversionException(Exception cause) {
+        super(cause);
+    }
+}
